9d8e59c76954585331fa6f1eb9fe5f290c776595
aysikh <amy.sikhammountry@gmail.com>
2021-02-28 15:46:37 -0500

    working on submit request for new user

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,9 +1,145 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button'
 
-export default function SignUpForm() {
-  return(
-    <div> 
-      <h1> sign up form is hurrrrr </h1> 
-    </div> 
+
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '1px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4),
+      display: "absolute",
+      left: "20%",
+      height: "80vh",
+      width: "120vh"
+    },
+  }))
+
+export default function SignUpForm(){ 
+  const history = useHistory();
+  const classes = useStyles();
+
+  const [ firstname, setFirstname ] = useState("")
+  const [ lastname, setLastname ] = useState("")
+  const [ email, setEmail ] = useState("")
+  const [ password, setPassword ] = useState("")
+  const [ errors, setErrors ] = useState( [] )
+
+  const handleFirstname = ( event ) => {
+    setFirstname( event.target.value )
+  }
+
+  const handleLastname = ( event ) => {
+    setLastname( event.target.value )
+  }
+  const handleEmail = ( event ) => {
+      setEmail( event.target.value )
+  }
+  const handlePassword = ( event ) => {
+      setPassword( event.target.value )
+  }
+
+  const handleSubmit = ( event ) => {
+    event.preventDefault();
+    const { firstname, lastname, email, password, password_confirmation } = this.state
+    let user = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password,
+    }
+
+    // axios.post( 'http://localhost:3001/users', { user }, { withCredentials: true } )
+    //   .then( response => {
+    //       if ( response.data.status === 'created' ) {
+    //           props.handleLogin( response.data )
+    //           redirect()
+    //       }
+    //       else {
+    //           setErrors( {
+    //               errors: response.data.errors
+    //           } )
+    //       }
+    //   } )
+    //   .catch( error => console.log( 'api errors:', error ) )
+    };
+
+    const handleNewUserSubmit = ( event ) => {
+      event.preventDefault()
+
+      let requestPackage = {
+          headers: { 'Content-Type': 'application/json' },
+          method: 'POST',
+          body: JSON.stringify( {
+              firstname: firstname,
+              lastname: lastname,
+              email: email,
+              password: password,
+          } )
+      }
+      fetch( "http://localhost:3001/users", requestPackage )
+          .then( rsp => rsp.json() )
+      // .then(console.log)
+      history.push( "/home" )
+  }
+
+
+  return (
+    <div>
+      <Container className={classes.paper}>
+        <Paper elevation={3}>
+          <form autoComplete="on"
+            noValidate
+            onSubmit={ ( event ) => {
+                handleNewUserSubmit( event )
+            } }>
+                                        
+            <TextField 
+              fullWidth
+              id="outlined-basic" 
+              label="First Name" 
+              variant="outlined" 
+            />
+            <br /> <br />
+            <TextField 
+              fullWidth
+              id="outlined-basic" 
+              label="Last Name" 
+              variant="outlined" 
+            />
+            <br /> <br />
+            <TextField 
+              fullWidth
+              id="outlined-basic" 
+              label="Email" 
+              variant="outlined" 
+            />
+            <br /> <br />
+            <TextField
+              fullWidth
+              id="outlined-multiline-static"
+              label="Password"
+              multiline
+              variant="outlined"
+            />
+            <br/> <br/> 
+            <Button 
+              type="submit" 
+              variant="contained" 
+              color="primary">
+              Submit
+            </Button> 
+          </form>
+        </Paper>
+        <br /> <br /> 
+      </Container>
+    </div>
   )
 }
--- src/containers/LandingPageContainer.js
+++ src/containers/LandingPageContainer.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Logo from '../assets/seen-logo-dummy.png'
+import SignUpForm from '../components/SignUpForm'
 
 export default function LandingPageContainer() {
   return (
-    <div> 
-      Hello I am the LANDING PAGE YEEET 
+    <div class="Landing Page"> 
+      <center> <img src={Logo} />  </center>
+      Want to know what Black women are saying about these tech companies?
+      <SignUpForm /> 
     </div> 
   )
 }
