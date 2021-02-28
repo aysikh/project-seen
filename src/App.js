228e41bb99f6d393d7eb0afd3d0a3a5ce7843995
aysikh <amy.sikhammountry@gmail.com>
2021-02-28 11:13:25 -0500

    created paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { CssBaseline, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import LandingPageContainer from './LandingPageContainer';
+import LogInContainer from './LogInContainer';
+import SignUpContainer from './SignUpContainer';
+import UserDashboardContainer from './UserDashboardContainer'
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const useStyles = makeStyles(() => ({
+  bg: {
+  minHeight: '100vh',
+  backgroundColor: '#EADCA7'
+  // backgroundImage: `url(${})`,
+  // backgroundRepeat: 'no-repeat',
+  // backgroundSize: 'cover',
+  },
+  appbarWrapper: {
+  width: '80%',
+  margin: '0px',
+  boxShadow: 'none'
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return(
+    <BrowserRouter>
+      <div className={classes.bg}>
+      <CssBaseline /> 
+        {/* <Toolbar className={classes.appbarWrapper} >  */}
+          <Navbar2 /> 
+        {/* </Toolbar> */}
+      <Switch>
+        <div>
+          <Route exact path="/" component={LandingPageContainer} />
+          <Route path="/home" component={UserDashboardContainer} />
+          <Route exact path="/signup" component={SignUpContainer} />
+          <Route path="/login" component={LogInContainer} /> 
+
+        </div>
+      </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
-
-export default App;
