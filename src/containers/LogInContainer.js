7e37d1f9e3f197f57d5cbabb9f4d9ec4dc0cdedd
aysikh <amy.sikhammountry@gmail.com>
2021-03-02 23:55:37 -0500

    added logincontainer and form

---
+++ src/containers/LogInContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'; 
+
+
+import LogInForm from '../components/LogInForm'
+
+export default function LogInContainer(){
+  return(
+    <div>
+      <LogInForm /> 
+    </div>
+  )
+}
