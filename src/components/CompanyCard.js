65b403cf1f769aaad187e9db93aa169f5a34fc18
aysikh <amy.sikhammountry@gmail.com>
2021-03-07 16:22:04 -0500

    added company size

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
+import BusinessIcon from '@material-ui/icons/Business';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PeopleIcon from '@material-ui/icons/People';
+
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: '25rem',
+    width: '28rem',
+    margin: '3.5rem'
+  },
   topInfo: {
     fontFamily: 'Josefin Sans',
     backgroundColor: theme.palette.background.paper,
@@ -26,6 +39,10 @@ const useStyles = makeStyles((theme) => ({
     height: "10%",
     width: "auto"
   },
+  stats: {
+    fontFamily: 'Cardo', 
+    display: "absolute",
+  }
 
 }))
 
@@ -34,16 +51,44 @@ export default function CompanyCard(props){
 
   return(
     <div className="company-info-container">
-      <div id="top-info" className={classes.topInfo}>
-        <img src={props.comp.logo} style={{height: '6rem'}}/> 
-        <h1 style={{fontSize: '3rem'}}>{props.comp.name}</h1>
+      <div id="company-name" className={classes.topInfo}>
+        <img src={props.comp.logo} style={{height: '6rem', marginTop: '.5rem', marginRight: '.5rem'}}/> 
+        <span style={{fontSize: '3rem'}}>{props.comp.name}</span>
+        <br /><br />
         <LocationOnIcon /> <span style={{fontSize: '1.25rem'}}>{props.comp.location}</span>
       </div>
       <div className={classes.description}>
         <h2 style={{color: 'white'}}>{props.comp.description}</h2>
       </div>
-      <div>
-        
+      <div id="company-stats" className={classes.stats}>
+      <Grid container className={classes.root} spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justify="center">
+            <Grid>
+              <Paper className={classes.paper} >
+                Demographic Chart hurrr
+              </Paper>
+            </Grid>
+            <Grid>
+              <Paper className={classes.paper} >
+                <center> 
+                  <PeopleIcon style={{fontSize: '10rem', color: 'gray'}}/>
+                  <h1>{props.comp.size} employees</h1>
+                </center>
+              </Paper>
+            </Grid>
+            <Grid>
+              <Paper className={classes.paper} >
+                <center>
+                  Rating stars here
+                  <br /> 
+                  Average score here
+                </center>
+              </Paper>
+            </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
       </div>
     </div>
   )
